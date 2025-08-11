0af4adf62e841a0e72100985abbc5b38b57fdad2
Renan YHUEL <renan.yhuel@gmail.com>
2025-08-11 14:55:21 +0200

    refactor: remove BasicLayout wrapper from RootLayout component

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { ReactNode } from 'react';
-import BasicLayout from '@/components/layouts/BasicLayout';
 import '../../styles/globals.css';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return <BasicLayout>{children}</BasicLayout>;
+  return <>{children}</>;
 }
